e806a1ee0ee29c072d2d38c0bd4ffdb90553f023
dc <00danzy@gmail.com>
2022-10-27 09:57:23 +0100

    first commit

---
+++ APPS/Flask & Jinja/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+   return render_template("index.html")
+
+
+@app.route("/blog")
+def blog():
+   return render_template("blog.html")
+
+
+@app.route("/user-profile")
+def profile():
+   return render_template("profile.html")
+
+
+if __name__ == "__main__":
+   app.run(debug=True)
+
