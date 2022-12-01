0d12b0b590ba648b7bffab00ef97d873830d0df7
dc <00danzy@gmail.com>
2022-12-01 22:52:31 +0100

    update

--- migrations/1666873779416_blogs-table.js
+++ migrations/1666873779416_blogs-table.js
@@ -7,9 +7,9 @@ exports.up = pgm => {
       CREATE TABLE blogs (
          id SERIAL PRIMARY KEY,
          title VARCHAR(150) NOT NULL,
-         content VARCHAR(1000) NOT NULL,
+         content VARCHAR(5000) NOT NULL,
          img VARCHAR(200) NOT NULL,
-         user_id INTEGER REFERENCES users(id) ON DELETE SET NULL,
+         user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
 
          created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
          updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
